722e4c151f56b2aaa35fd5dfbf23012e34e9f004
Fabian Fetting <mail@fabianfetting.de>
2016-12-04 11:01:21 +0100

    Initial commit

---
+++ tasks/cordova_api.js
@@ -0,0 +1,18 @@
+/*
+ * grunt-cordova-api
+ * https://github.com/fur6y/grunt-cordova-api
+ *
+ * Copyright (c) 2016 Fabian Fetting
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('cordova', 'Description.', function() {
+      // Print a success message.
+      grunt.log.ok();
+  });
+
+};
